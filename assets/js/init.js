577aba3df76bdfb21aa0e9a93ef0675b966c4a59
sostdk <morten@sost.dk>
2018-09-10 13:56:59 +0200

    First

---
+++ assets/js/init.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $('.contactmodal').modal();
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: 'true',
+        indicators: 'true',
+        duration: 300
+    });
+    setTimeout(autoplay, 4500);
+    function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+    }
+
+
+    $('.sidenav').sidenav({
+        menuWidth:300,
+        edge: 'right'
+    });
+
+
+    $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+    });
+    
+    $(".load-animate").addClass("loaded");  
+    
+});
